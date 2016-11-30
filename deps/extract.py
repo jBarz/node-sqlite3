893ba427675fa2cadde3f6d2ca3d7c76f7f2be47
jBarz <jBarz@users.noreply.github.com>
2016-11-30 16:23:14 -0500

    xlc options on zOS instead of gcc
    Conflicts:
    	binding.gyp
    	deps/extract.py

--- deps/extract.py
+++ deps/extract.py
@@ -4,6 +4,9 @@ import os
 
 tarball = os.path.abspath(sys.argv[1])
 dirname = os.path.abspath(sys.argv[2])
-tfile = tarfile.open(tarball,'r:gz');
-tfile.extractall(dirname)
+if platform.system() == 'OS/390':
+  os.system("cd %s && (iconv -f IBM-1047 -t ISO8859-1 %s | gunzip -c > temp.tar) && pax -ofrom=ISO8859-1,to=IBM-1047 -rf temp.tar" % (dirname, tarball))
+else:
+  tfile = tarfile.open(tarball,'r:gz');
+  tfile.extractall(dirname)
 sys.exit(0)
